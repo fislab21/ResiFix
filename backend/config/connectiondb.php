6fe5c68ff37ffe12aa73efb72c280ea7cc3ea846
abdou21fsl <fisliabderrahmane@gmail.com>
2026-03-03 16:08:27 +0100

    --

--- backend/config/connectiondb.php
+++ backend/config/connectiondb.php
@@ -8,7 +8,7 @@ $dbname = "resifix";
 try{
     $conn =  new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully
+     echo "Connected successfully";
 }
 catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
